02e6f99451ee9c2fa2d9b4c2d8c17709bdf6db6e
ASUS <530056048@qq.com>
2019-07-15 14:37:28 +0800

    ok

--- lcf/src/main/java/com/bw/MyMaven.java
+++ lcf/src/main/java/com/bw/MyMaven.java
@@ -3,7 +3,7 @@ package com.bw;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//1234
 @SpringBootApplication
 @MapperScan("com.bw.mapper")
 public class MyMaven {
